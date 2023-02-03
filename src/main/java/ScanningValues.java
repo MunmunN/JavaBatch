3121c18f823fba197e3a557e5769dead0c7c1a8e
MunmunN <nandi.munmun2015@gmail.com>
2023-02-02 21:40:25 -0500

    first commit

---
+++ src/main/java/ScanningValues.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class ScanningValues {
+  public static void main(){
+    Scanner sc =new Scanner(System.in);
+    System.out.println("pl enter value of a");
+    int a = sc.nextInt();
+    System.out.println("pl enter value of b");
+    int b =sc.nextInt();
+    if ( a==b){
+      System.out.println("a=b");
+
+    }
+    else{
+      System.out.println("a and b not same");
+    }
+
+
+  }
+
+
+}
